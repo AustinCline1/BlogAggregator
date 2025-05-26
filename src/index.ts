05cc636b7cf8210b8f58418ffee43307bb7d531d
Austin Cline <au.cline@hotmail.com>
2025-05-26 18:16:12 -0400

    Added config files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-﻿function main() {
+﻿import {readConfig, setUser} from "./config";
+function main() {
     console.log("Hello, world!");
+    setUser("Austin");
+    const config =  readConfig();
+    console.log(config);
+
 }
 
 main();
